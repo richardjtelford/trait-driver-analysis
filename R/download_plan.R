42e295eb2d40a5ab6647e56b603143204411cd5c
richardjtelford <richard.telford@bio.uib.no>
2020-04-15 18:29:50 +0200

    uses new dataDownloader

--- R/download_plan.R
+++ R/download_plan.R
@@ -1,33 +1,81 @@
 ## download data
 
-##TODO - 
+##TODO -
 # trigger on osf change TRAIT COMMUNITY CLIMATE
 
-#function to download from osf and return file path
-download <- function(node, remote_path = ".", file, path = "data"){
-  get_file(node = node, remote_path = remote_path, file = file, path = path)
-  file.path(path, file)
-  } 
-
-
 download_plan <- drake_plan(
-  
   #community
   community_download = target(
-    download(node = "f3knq", remote_path = "Community", file = "transplant.sqlite"),
-    format = "file"), 
+    get_file(
+      node = "f3knq",
+      remote_path = "Community",
+      file = "transplant.sqlite",
+      path = "data"
+    ),
+    format = "file",
+    trigger = trigger(
+      condition = need_update(
+        node = "f3knq",
+        remote_path = "Community",
+        file = "transplant.sqlite",
+        path = "data"
+      )
+    )
+  ),
   
   #import trait data
   traits_leaf_download = target(
-    download(node = "f3knq", remote_path = "Traits", file = "PFTC1.2_China_2015_2016_LeafTraits.csv"),
-    format = "file"),
+    get_file(
+      node = "f3knq",
+      remote_path = "Traits",
+      file = "PFTC1.2_China_2015_2016_LeafTraits.csv",
+      path = "data"
+    ),
+    format = "file",
+    trigger = trigger(
+      condition = need_update(
+        node = "f3knq",
+        remote_path = "Traits",
+        file = "PFTC1.2_China_2015_2016_LeafTraits.csv",
+        path = "data"
+      )
+    )
+  ),
   
   traits_chemical_download = target(
-    download(node = "f3knq", remote_path = "Traits", file = "PFTC1.2_China_2015_2016_ChemicalTraits.csv"), 
-    format = "file"),
-
+    get_file(
+      node = "f3knq",
+      remote_path = "Traits",
+      file = "PFTC1.2_China_2015_2016_ChemicalTraits.csv",
+      path = "data"
+    ),
+    format = "file",
+    trigger = trigger(
+      condition = need_update(
+        node = "f3knq",
+        remote_path = "Traits",
+        file = "PFTC1.2_China_2015_2016_ChemicalTraits.csv",
+        path = "data"
+      )
+    )
+  ),
+  
   #import environmental data
   env_download = target(
-    download(node = "4hjzu", remote_path = ".", file = "climate_month.Rdata"), 
-    format = "file")
+    get_file(
+      node = "4hjzu",
+      remote_path = ".",
+      file = "climate_month.Rdata",
+      path = "data"
+    ),
+    format = "file",
+    trigger = trigger(
+      condition = need_update(
+        node = "4hjzu",
+        remote_path = ".",
+        file = "climate_month.Rdata",
+        path = "data"
+      )
+    )
+  )
 )
