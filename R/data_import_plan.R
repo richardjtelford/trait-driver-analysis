b205219c56156750c2b11aa1c3fd76d43ceae9c3
richardjtelford <richard.telford@bio.uib.no>
2020-04-20 15:59:00 +0200

    process imported daa

--- R/data_import_plan.R
+++ R/data_import_plan.R
@@ -7,29 +7,42 @@
 import_plan <- drake_plan(
   
   #import community
-  community = community_download, 
-  
-  #import trait data
-  traits0 = {
-      
+  community = {
+    #make community database connection
+    con = DBI::dbConnect(RSQLite::SQLite(), community_download, create = FALSE)
     #import
-    leaf_traits = read_csv(traits_leaf_download)
-    chemical_traits = read_csv(traits_chemical_download)
-    },
+    load_comm(con = con)
+    }, 
+
+  #import trait data
+  traits_leaf0 = read_csv(traits_leaf_download, col_types = cols(StoichLabel = col_character())),
+  traits_chemical0 = read_csv(traits_chemical_download),
   
-  traits = traits0 %>% 
-    select(-Full_Envelope_Name, -Envelope_Name_Corrected, -Date, -Elevation, -P_FILE_NAME, -matches("Flag$"), -allComments, -FileName, -Taxon_written_on_envelopes, -CN_FILE_NAME , -StoichLabel, -P_Std_Dev, -P_Co_Var, -LeafID) %>% 
+  traits_leaf = traits_leaf0 %>% 
+    select(-Envelope_Name_Corrected, -Date, -matches("Flag$"), -allComments) %>% 
+    #remove leaf-thickness measurement (keep mean)
     select(-matches("Leaf_Thickness_\\d_mm")) %>% 
-    pivot_longer(cols = -(Site:Leaf_number), names_to = "trait", values_to = "value") %>% 
-    filter(!is.na(value)) %>% 
-    mutate(Site = factor(Site, levels = levels(community$originSiteID))),
+    filter() %>% 
+    pivot_longer(cols = c("Wet_Mass_g", "Dry_Mass_g", "Leaf_Thickness_Ave_mm", "Leaf_Area_cm2", "SLA_cm2_g", "LDMC"), names_to = "trait", values_to = "value"),
+  
+  traits_chemical = traits_chemical0 %>% 
+    select(-Date, -n) %>%
+    filter() %>% 
+    pivot_longer(cols = c("P_percent", "C_percent", "N_percent", "CN_ratio", "dN15_permil", "dC13_permil"), names_to = "trait", values_to = "value"),
+  
+  #combine leaf and chemical traits
+  traits0 = bind_rows(traits_leaf,
+                     traits_chemical) %>%
+    filter(!is.na(value)) %>%
+    mutate(Site = factor(Site, levels = levels(community$originSiteID))), 
+  
+  
   #TODO clean impossible trait values using BIEN
   #calculate derived traits
   #transform
+  traits = traits0 %>% 
+    rename(Location = destBlockID),
   
   #import environmental data
-  env = {
-    #import
-    get(load(env_download))
-    }
+  env = get(load(env_download))
 )
