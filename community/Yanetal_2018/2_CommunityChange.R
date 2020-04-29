3fe4f8998f7d8c5468bb01a4fc1d7e80197b2a9a
audhalbritter <audhalbritter.com>
2020-04-29 09:03:55 +0200

    delete community folder

---
+++ community/Yanetal_2018/2_CommunityChange.R
@@ -0,0 +1,431 @@
+##load packages
+library("tidyverse")
+library("vegan")
+library("ggvegan")
+library("lubridate")
+library("gridExtra")
+library("cowplot")
+library("nlme")
+library("broom")
+
+source("community/start_here.R")
+source("community/Yanetal_2018/yans_ms_functions.R")
+
+
+##load data
+#climate
+load("community/Yanetal_2018/data/climate_month.Rdata")
+summer_temp <- climate_month %>% 
+  filter(variable == "Tair") %>% 
+  mutate(month = month(month)) %>%
+  filter(month %in% 6:8) %>% 
+  group_by(logger, site) %>% 
+  summarise(mean = mean(value, na.rm = TRUE)) %>% 
+  arrange(site) %>%
+  ungroup()
+
+#control = Tair
+#OTC = OTC - Tair
+#Warm1 = TairD - TairO  
+
+pred_temp <- bind_rows(
+  summer_temp %>% 
+    filter(logger == "gradient") %>% 
+    mutate(TTtreat = "control"),#control
+  summer_temp %>% 
+    filter(logger == "gradient") %>% 
+    mutate(TTtreat = "local"),#local transplant (== control)
+  summer_temp %>% 
+    spread(key = logger, value = mean) %>% 
+    mutate(mean = otc - gradient, TTtreat = "OTC") %>% #replaced with default later
+    select(-gradient, -otc),
+  summer_temp %>% 
+    filter(logger == "gradient") %>% 
+    mutate(mean = lead(mean) - mean, TTtreat = "warm1") %>% 
+    filter(site != "L")
+)  %>% 
+  select(-logger) %>%
+  mutate(mean = if_else(TTtreat == "OTC", 1.5, mean))
+warning("using estimated OTC effect for all sites")
+
+
+#community
+cover_thin <- cover_thin %>% 
+  filter(TTtreat %in% c("control", "local", "warm1", "OTC"))
+
+#turf environment
+turf_env <- tbl(con, "turfEnvironment") %>% collect()
+
+
+## functional groups
+fun_gp <- tbl(con, "taxon") %>% 
+  select(species, functionalGroup) %>% 
+  collect()
+
+
+## Calculate responses
+responses <- cover_thin %>% 
+  left_join(fun_gp) %>% 
+  left_join(turf_env) %>% 
+  group_by(turfID, originBlockID, TTtreat, originSiteID, year) %>%  
+  summarise(richness = n(), 
+            diversity = diversity(cover), 
+            N1 = exp(diversity),
+            evenness = diversity/log(richness),
+            sumCover = sum(cover),
+            propGraminoid = sum(cover[functionalGroup %in% c("gramineae", "sedge")])/sumCover,
+            total_vascular = first(totalVascular),
+            vegetationHeight = mean(vegetationHeight)
+  ) %>% 
+  group_by(originBlockID, TTtreat, originSiteID, year) %>%
+  select(-turfID) %>% 
+  full_join(pred_temp, by = c("originSiteID" = "site", "TTtreat" = "TTtreat")) %>% 
+  ungroup() %>% 
+  mutate(contrast = if_else(TTtreat %in% c("local", "control"), 0, mean), 
+         TTtreat = factor(TTtreat, levels = c("control", "local", "warm1", "OTC"))) 
+
+
+# make separate data sets for 2016 and different treatments
+Gradient <- responses %>% 
+  filter(year == 2016, TTtreat %in% c("local", "control")) 
+OTC <- responses %>% 
+  filter(year == 2016, TTtreat %in% c("local", "OTC")) 
+Transplant <- responses %>% 
+  filter(year == 2016, TTtreat %in% c("local", "warm1")) 
+
+Transplant <- bind_rows(Transplant = Transplant, Gradient = Gradient, OTC = OTC, .id = "experiment")
+
+# function to get aic values
+## ---- regressions_lines
+augment_aic <- function(x){
+  augment(x) %>% 
+    mutate(aic = AIC(x))
+}
+
+# compare different models
+gradient <- responses %>% 
+  filter(year == 2016, TTtreat %in% c("local", "control")) %>% 
+  gather(key = variable, value = value, -(originBlockID:year), -mean, -contrast) %>% 
+  filter(!is.na(value)) %>% 
+  group_by(variable) %>% 
+  # mutate(value = scale(value)) %>% 
+  do(mod0 = lm(value ~ 1 + mean - mean, data = .),
+     mod1 = lm(value ~ mean, data = .),
+     mod2 = lm(value ~ mean, data = .) # to have the same number of models (not sure if needed)
+  )
+
+gradient %>% 
+  ungroup() %>% 
+  group_by(variable) %>%
+  do(AIC(.$mod0[[1]], .$mod1[[1]]))
+
+
+transplant <- responses %>%
+  filter(year == 2016, TTtreat %in% c("local", "warm1"), originSiteID != "L") %>% 
+  gather(key = variable, value = value, -(originBlockID:year), -mean, -contrast) %>% 
+  filter(!is.na(value)) %>% 
+  group_by(variable) %>% 
+  #  mutate(value = scale(value)) %>% 
+  do(mod0 = lm(value ~ originSiteID + contrast - contrast, data = .),
+     mod1 = lm(value ~ contrast + originSiteID, data = .),
+     mod2 = lm(value ~ contrast * originSiteID, data = .)
+  )
+
+transplant %>% 
+  ungroup() %>% 
+  group_by(variable) %>%
+  do(AIC(.$mod0[[1]], .$mod1[[1]], .$mod2[[1]])) %>% 
+  spread(key = df, value = AIC)
+
+
+otc <- responses %>%
+  filter(year == 2016, TTtreat %in% c("local", "OTC")) %>% 
+  gather(key = variable, value = value, -(originBlockID:year), -mean, -contrast) %>% 
+  filter(!is.na(value)) %>% 
+  group_by(variable) %>% 
+  #mutate(value = scale(value)) %>% 
+  do(mod0 = lm(value ~ originSiteID + contrast - contrast, data = .),
+     mod1 = lm(value ~ contrast + originSiteID, data = .),
+     mod2 = lm(value ~ contrast * originSiteID, data = .)
+  )
+
+otc %>% 
+  ungroup() %>% 
+  group_by(variable) %>%
+  do(AIC(.$mod0[[1]], .$mod1[[1]], .$mod2[[1]])) %>% 
+  spread(key = df, value = AIC)
+
+# bind model outputs and filter for relevant models, tidy up
+augmented_reg <- 
+  bind_rows(Transplant = transplant, OTC = otc, Gradient = gradient, .id = "experiment") %>% 
+  ungroup() %>% 
+  group_by(variable, experiment) %>%
+  do(
+    bind_rows(
+      null = augment_aic(.$mod0[[1]]), 
+      effect = augment_aic(.$mod1[[1]]), 
+      interaction = augment_aic(.$mod2[[1]]), 
+      .id = "model")
+  ) %>% 
+  filter(!(experiment == "Gradient" & model == "interaction")) %>% 
+  filter(aic == min(aic)) %>% 
+  ungroup() %>% 
+  rename(response = "variable") %>% 
+  filter(response %in% c("richness", "evenness", "propGraminoid")) %>%
+  mutate(
+    originSiteID = plyr::mapvalues(originSiteID, c("H", "A", "M", "L"), c("High alpine", "Alpine", "Middle", "Lowland")),
+    response = plyr::mapvalues(response, c("richness", "evenness", "propGraminoid"), c("Richness", "Evenness", "Proportion Graminoid")),
+    response = factor(response, levels = c("Richness", "Evenness", "Proportion Graminoid")),
+    contrast = if_else(experiment == "Gradient", mean, contrast),
+    model = factor(model, levels = c("null", "effect", "interaction"), labels = c("No effect", "Effect", "Interaction")))
+
+
+
+# preprocess points for plot
+dd <- Transplant %>% 
+  select(-diversity, -N1, -total_vascular) %>% 
+  mutate(xvalue = ifelse(experiment == "Gradient", mean, contrast)) %>% 
+  gather(key = response, value = value, richness, evenness, propGraminoid) %>% 
+  mutate(response = plyr::mapvalues(response, c("richness", "evenness", "propGraminoid"), c("Richness", "Evenness", "Proportion Graminoid"))) %>% 
+  mutate(response = factor(response, levels = c("Richness", "Evenness", "Proportion Graminoid"))) %>% 
+  mutate(dummycolor = ifelse(experiment == "Gradient", "Gradient", as.character(originSiteID))) %>% 
+  mutate(originSiteID = plyr::mapvalues(originSiteID, c("H", "A", "M", "L"), c("High alpine", "Alpine", "Middle", "Lowland"))) %>% 
+  mutate(originSiteID = factor(originSiteID, levels = c("High alpine", "Alpine", "Middle", "Lowland"))) %>% 
+  mutate(TTtreat = plyr::mapvalues(TTtreat, c("warm1", "local", "control", "OTC"), c("Transplant", "Local transplant", "Control", "OTC"))) %>% 
+  mutate(TTtreat = factor(TTtreat, levels = c("Control", "Local transplant", "OTC", "Transplant"))) %>% 
+  filter(!(experiment == "Transplant" & originSiteID == "Lowland"))
+
+# plot Figure 1
+p <- ggplot(dd, aes(x = xvalue, y = value, colour = originSiteID, shape = TTtreat)) + 
+  geom_jitter(height = 0, width = 0.1, size = 1.8) +
+  geom_line(data = filter(augmented_reg, experiment != "Gradient"), aes(y = .fitted, x = contrast, colour = originSiteID, linetype = model), inherit.aes = FALSE) +
+  geom_line(data = filter(augmented_reg, experiment == "Gradient") , aes(y = .fitted, x = contrast, linetype = model), colour = "grey40", inherit.aes = FALSE) +
+  facet_grid(response ~experiment, scales = "free", space = "free_x") +
+  scale_x_continuous(breaks = c(0,1,2,7,8,9,10,11,12)) +
+  scale_color_brewer(palette = "RdBu", direction = -1) +
+  scale_shape_manual(values = c(1, 16, 18, 17)) +
+  scale_linetype_manual(values = c("dotted", "dashed", "solid")) + 
+  labs(x = "", y = "", colour = "Site", shape = "Treatment", linetype = "Model") +
+  theme_light() +
+  theme(panel.grid.major = element_blank(), 
+        #panel.grid.minor = element_blank(),
+        panel.border = element_rect(colour = "grey", fill=NA, size=1))
+
+CommunityPlot <- ggdraw(p) + 
+  draw_label("Number of species", x = 0.01, y = 0.925, angle = 90,
+           vjust = 1, hjust = 1, size = 14) +
+  draw_label("Index", x = 0.01, y = 0.52, angle = 90,
+             vjust = 1, hjust = 1, size = 14) +
+  draw_label("Proportion", x = 0.01, y = 0.3, angle = 90,
+             vjust = 1, hjust = 1, size = 14) +
+  draw_label("Temperature °C", x = 0.36, y = 0.03,
+             vjust = 1, hjust = 1, size = 14) +
+  draw_label("Contrasts °C", x = 0.625, y = 0.03,
+             vjust = 1, hjust = 1, size = 14) +
+  draw_label("Contrasts °C", x = 0.795, y = 0.03,
+             vjust = 1, hjust = 1, size = 14)
+CommunityPlot
+#ggsave(CommunityPlot, filename = "community/FinalFigures/CommunityPlot.jpg", height = 8, width = 10, dpi = 300)
+
+
+
+### Vegetation height
+dat <- cover_thin %>% 
+  left_join(fun_gp) %>% 
+  left_join(turf_env) %>%  
+  filter(year == "2013") %>% 
+  mutate(newTT = plyr::mapvalues(TTtreat, c("warm1", "control", "local", "OTC"), c("warm1", "control", "control", "OTC"))) %>% 
+  group_by(destSiteID, TTtreat) %>% 
+  summarise(n = n(), meanH = mean(vegetationHeight), se = sd(vegetationHeight)/sqrt(n)) %>% 
+  filter(!TTtreat %in% c("warm1", "local"))
+
+dat %>% 
+  unite(col = MeanSE, c("meanH", "se"), sep = "_") %>% 
+  select(-n) %>% 
+  spread(key = TTtreat, value = MeanSE) %>% 
+  separate(col = control, into = c("MeanC", "SeC"), sep = "_", convert = TRUE) %>% 
+  separate(col = OTC, into = c("MeanOTC", "SeOTC"), sep = "_", convert = TRUE) %>% 
+  mutate(diff = MeanOTC - MeanC, diffSE = sqrt(SeOTC^2 + SeC^2))
+  
+dd <- cover_thin %>% 
+  left_join(fun_gp) %>% 
+  left_join(turf_env) %>%  
+  filter(year == "2013") %>% 
+  mutate(newTT = plyr::mapvalues(TTtreat, c("warm1", "control", "local", "OTC"), c("warm1", "control", "control", "OTC"))) %>% 
+  group_by(destPlotID, destSiteID, TTtreat) %>% 
+  summarise(n = n(), meanH = mean(vegetationHeight), se = sd(vegetationHeight)/sqrt(n)) %>% 
+  filter(!TTtreat %in% c("warm1", "local"))
+fit <- lm(meanH ~ TTtreat, dd)
+summary(fit)
+anova(fit)
+plot(fit)
+
+
+### Test model assumptions
+grad <- responses %>% 
+  filter(year == 2016, TTtreat %in% c("local", "control")) %>% 
+  gather(key = variable, value = value, -(originBlockID:year), -mean, -contrast) %>% 
+  filter(!is.na(value))
+
+trans <- responses %>%
+  filter(year == 2016, TTtreat %in% c("local", "warm1"), originSiteID != "L") %>% 
+  gather(key = variable, value = value, -(originBlockID:year), -mean, -contrast) %>% 
+  filter(!is.na(value))
+
+otcs <- responses %>%
+  filter(year == 2016, TTtreat %in% c("local", "OTC")) %>% 
+  gather(key = variable, value = value, -(originBlockID:year), -mean, -contrast) %>% 
+  filter(!is.na(value))
+
+dat <- bind_rows(Transplant = trans, OTC = otcs, Gradient = grad, .id = "experiment") %>% 
+  ungroup() %>% 
+  filter(variable %in% c("richness", "evenness", "propGraminoid"))
+
+plyr::d_ply(dat, plyr::.(experiment, variable), function(df){
+  y1 <- quantile(df$value[!is.na(df$value)], c(0.25, 0.75))
+  x1<- qnorm(c(0.25, 0.75))
+  slope <- diff(y1)/diff(x1)
+  int <- y1[1L] - slope * x1[1L]
+  
+  print(
+    ggplot(df, aes(sample = value)) +
+      stat_qq() +
+      geom_abline(slope = slope, intercept = int) +
+      ggtitle(paste(df$experiment[1], df$variable[1], sep = "-"))
+  )
+  cat("\n")
+})
+
+
+
+gradient <- responses %>% 
+  filter(year == 2016, TTtreat %in% c("local", "control"))
+mod1 = lm(propGraminoid ~ mean, data = gradient)
+plot(mod1)
+
+transplant <- responses %>%
+  filter(year == 2016, TTtreat %in% c("local", "warm1"), originSiteID != "L")
+mod0 = lm(propGraminoid ~ originSiteID + contrast - contrast, data = transplant)
+plot(mod0)
+mod1 = lm(propGraminoid ~ contrast + originSiteID, data = transplant)
+plot(mod1)
+mod2 = lm(evenness ~ contrast * originSiteID, data = transplant)
+plot(mod2)
+
+otc <- responses %>%
+  filter(year == 2016, TTtreat %in% c("local", "OTC"))
+mod0 = lm(richness ~ originSiteID + contrast - contrast, data = otc)
+plot(mod0)
+mod1 = lm(richness ~ contrast + originSiteID, data = otc)
+plot(mod1)
+mod2 = lm(propGraminoid ~ contrast * originSiteID, data = otc)
+plot(mod2)
+
+
+
+# table of effects
+## ---- regressions
+gradient <- responses %>% 
+  filter(year == 2016, TTtreat %in% c("local", "control")) %>% 
+  gather(key = variable, value = value, -(originBlockID:year), -mean, -contrast) %>% 
+  filter(!is.na(value)) %>% 
+  group_by(variable) %>% 
+  mutate(value = scale(value)) %>% 
+  do({
+    mod = lme(value ~ mean, random =  ~ 1|originSiteID, data = .)
+    tidy(mod, effects = "fixed")
+  })
+
+transplant <- responses %>%
+  filter(year == 2016, TTtreat %in% c("local", "warm1"), originSiteID != "L") %>% 
+  gather(key = variable, value = value, -(originBlockID:year), -mean, -contrast) %>% 
+  filter(!is.na(value)) %>% 
+  group_by(variable) %>% 
+  mutate(value = scale(value)) %>% 
+  do({
+    mod = lme(value ~ contrast, random =  ~ 1|originSiteID, data = .)
+    tidy(mod, effects = "fixed")
+  })
+
+otc <- responses %>%
+  filter(year == 2016, TTtreat %in% c("local", "OTC")) %>% 
+  gather(key = variable, value = value, -(originBlockID:year), -mean, -contrast) %>% 
+  filter(!is.na(value)) %>% 
+  group_by(variable) %>% 
+  mutate(value = scale(value)) %>% 
+  do({
+    mod = lme(value ~ contrast, random =  ~ 1|originSiteID, data = .)
+    tidy(mod, effects = "fixed")
+  })
+
+effects <- bind_rows(Gradient = gradient, Transplant = transplant, OTC = otc, .id = "Experiment") %>% 
+  mutate(Experiment = factor(Experiment, levels = c("Gradient", "OTC", "Transplant" ))) %>% 
+  filter(term != "(Intercept)") 
+
+
+EffectPlot <- effects %>% 
+  filter(!variable %in% c("N1", "diversity", "total_vascular")) %>% 
+  ungroup() %>% 
+  mutate(variable = plyr::mapvalues(variable, c("richness", "evenness", "sumCover", "propGraminoid"), c("Richness", "Evenness", "Sum of Cover", "Proportion Graminoid"))) %>% 
+  mutate(variable = factor(variable, levels = c("Richness", "Evenness", "Sum of Cover", "Proportion Graminoid"))) %>% 
+  ggplot(aes(x = Experiment, y = estimate, ymax = estimate + 2 * std.error, ymin = estimate - 2 * std.error, shape = Experiment, color = Experiment)) + 
+  geom_point(size = 3) +
+  geom_errorbar(width = 0.1) +
+  geom_hline(yintercept = 0, linetype = "dashed", colour = "grey50") +
+  scale_shape_manual(values = c(16, 18, 17)) +
+  scale_color_manual(values = c("grey", "purple", "orange")) +
+  labs(y = "Estimate", x = "") +
+  facet_wrap(~ variable) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.position = "none")
+
+ggsave(EffectPlot, filename = "community/FinalFigures/EffectPlot.jpg", height = 5, width = 5, dpi = 300)
+
+
+effects_Table <- bind_rows(Gradient = gradient, Transplant = transplant, OTC = otc, .id = "Experiment") %>% 
+  mutate(Experiment = factor(Experiment, levels = c("Gradient", "OTC", "Transplant" ))) %>% 
+  ungroup %>% 
+  filter(variable %in% c("richness", "evenness", "sumCover", "propGraminoid")) %>% 
+  mutate(variable = plyr::mapvalues(variable, c("richness", "evenness", "sumCover", "propGraminoid"), c("Richness", "Evenness", "Sum of Cover", "Proportion Graminoid"))) %>% 
+  mutate(variable = factor(variable, levels = c("Richness", "Evenness", "Sum of Cover", "Proportion Graminoid"))) %>% 
+  mutate(term = plyr::mapvalues(term, c("(Intercept)", "mean", "contrast"), c("Intercept", "Slope", "Slope"))) %>% 
+  select(-std.error) %>% 
+  mutate(estimate = round(estimate, 2), statistic = round(statistic, 2), p.value = round(p.value, 3)) %>% 
+  rename(Response = variable, Term = term, Estimate = estimate, Statistic = statistic, P.value = p.value)
+
+write.csv(effects_Table, "Effects_Table.csv", row.names = FALSE)
+
+
+
+#### Colonization Plot
+
+# dest controls in 2016 compared to origin control in 2012
+destC16 <- cover_thin %>% 
+  filter(year %in% c(2016)) %>% # first and last year
+  filter(TTtreat %in% c("control")) %>% 
+  select(turfID, destSiteID, TTtreat, species, cover)
+
+temp <- cover_thin %>% 
+  filter(year %in% c(2012)) %>% # first and last year
+  filter(TTtreat %in% c("control")) %>% 
+  select(turfID, originSiteID, TTtreat, species, cover) %>% 
+  full_join(destC16, by = c("originSiteID" = "destSiteID", "turfID", "TTtreat", "species"), suffix = c(".12", ".16")) %>% 
+  filter(is.na(cover.12)) %>% 
+  mutate(TTtreat = "temporal") %>% 
+  rename(First = cover.12, Last = cover.16)
+
+
+cover_thin %>% 
+  filter(year %in% c(2012, 2016)) %>% # first and last year
+  filter(TTtreat %in% c("control", "local", "warm1", "OTC")) %>% # 4 treatments
+  mutate(year = plyr::mapvalues(year, c(2012, 2016), c("First", "Last"))) %>% 
+  spread(key = year, value = cover) %>% # spread by year
+  select(turfID, originSiteID, TTtreat, species, First, Last) %>% 
+  filter(is.na(First)) %>% # filter all species which were not there in the first year
+  bind_rows(temp) %>% 
+  ggplot(aes(x = TTtreat, y = Last)) +
+  geom_boxplot() +
+  facet_wrap(~ originSiteID)
+  
+
+
+  
---
+++ community/databaseSetup/1_xls_to_csv.R
@@ -0,0 +1,154 @@
+####
+####Convert xls files to single csv file
+####
+
+#list of xls files
+flist <- dir("community/databaseSetup/data/commXLSx", pattern = "*.xls", full.names = TRUE, recursive = TRUE)
+
+#what do we have
+sapply(flist, excel_sheets)
+
+originDestination <- read.table(header = TRUE, stringsAsFactors = FALSE, text = "
+  origin, destination
+  HOTC HOTC
+  AOTC AOTC
+  MOTC MOTC
+  LOTC LOTC
+
+  HC HC
+  AC AC
+  MC MC
+  LC LC
+
+  HO HO
+  AO AO
+  MO MO
+  LO LO
+
+  H1 A1
+  A1 M1
+  M1 L1
+
+  A2 H2
+  M2 A2
+  L2 M2
+
+  H3 L3
+
+  L4 H4
+")  
+
+#read taxonomy file
+taxonomy <- readr::read_csv("community/databaseSetup/data/transplant_taxonomy.csv")
+taxonomy <- taxonomy %>% 
+  filter(!is.na(fullName)) # I hate excel
+
+#sanity checks on taxonomy file
+dups <- taxonomy %>% filter(is.na(keep)) %>% group_by(newCode) %>% filter(n() > 1) %>% arrange(newCode)
+assert_that(nrow(dups) == 0)
+
+taxonomy <- taxonomy %>% 
+  select(oldCode, newCode)
+                                
+#import data, process and export to CSV
+allsites <- plyr::ldply(flist, function(fl){
+  print(fl)
+  onesite <- plyr::ldply(excel_sheets(fl), function(sheet){
+    print(sheet)
+    dat <- read_excel(fl, sheet = sheet)
+    names(dat) <- gsub("__\\d$", "", names(dat)) #force duplicate columns to have same name. Probably won't work with future versions of tibble
+
+    #fix metadata
+    dat <- dat[!is.na(dat$Measure), ]
+    meta <- dat$DestinationSite #NB DestinationSite is the OriginSite!
+    meta[is.na(meta)] <- meta[which(is.na(meta))-1]#fill blanks in meta with row above   
+    dat$originPlotID <-meta
+    dat$turfID <- meta
+    dat$TTtreat <- substr(meta, 4, nchar(meta))
+  
+    
+
+#    dat$destinationPlotID <- meta    
+
+    #find destination site/block/plot
+    sitetreat <-  gsub("[[:digit:]]-", "", meta) #remove block info
+    destSitetreat <- plyr::mapvalues(sitetreat, from = originDestination$origin, to = originDestination$destination, warn_missing = FALSE)
+    dat$destinationPlotID <- paste0(substr(destSitetreat, 1, 1), substr(meta,2, 3), dat$TTtreat)
+    dat$DestinationSite <- substr(destSitetreat, 1, 1)
+    dat$DestinationBlock <- substr(dat$destinationPlotID, 1, 2)#assuming transplant from block1 go to block 1
+    
+
+    #delete empty columns
+    to_delete <- which(colSums(!is.na(dat)) == 0)
+    to_delete <- to_delete[names(to_delete) != "comment"]
+    dat <- dat[, -to_delete] # currently also removes GRtreat and RTtreat
+    
+    #year
+    dat$year[is.na(dat$year)] <- dat$year[which(is.na(dat$year)) - 1]#fill blanks in year with row above
+    #recorder
+    dat$recorder[is.na(dat$recorder)] <- dat$recorder[which(is.na(dat$recorder)) - 1]#fill blanks in year with row above
+    
+    #taxonomy
+    names(dat) <- trimws(names(dat))#zap trailing white space
+    names(dat) <- make.names(names(dat))
+    extras <- setdiff(names(dat), taxonomy$oldCode)
+    taxonomy <- rbind(taxonomy, cbind(oldCode = extras, newCode = extras))
+    names(dat) <- plyr::mapvalues(names(dat), from = taxonomy$oldCode, to = taxonomy$newCode, warn_missing = FALSE) 
+  
+    #deal with multiple columns
+    multiple <- plyr::count(names(dat))
+    multiple <- multiple$x[multiple$freq > 1]
+    if(length(multiple) > 0) {
+      sppX <- lapply(multiple, function(sp) {
+        rowSums(dat[, names(dat) == sp], na.rm = TRUE)
+      })
+      sppX <- setNames(as.data.frame(sppX), multiple)
+      dat <- cbind(dat[!names(dat) %in% multiple], sppX)
+    }
+    assert_that(all(table(names(dat)) == 1))#check no duplicates
+    
+    #assume correction file if no comments
+    if(grepl("corrections/", fl)){
+      dat$comment <- "Yans correction"# use this for flag
+    }
+    
+    #check date in date format
+    if(class(dat$date)[1] == "numeric"){
+      dat$date <- as.POSIXct(dat$date, origin = "1900-1-01")
+    }
+      
+    dat
+  })
+  onesite
+})
+
+assert_that(!any(is.na(allsites$turfID)))# check for missing values
+
+#sort columns so all species together
+metaNames <- c("DestinationSite", "DestinationBlock", "originPlotID", "TTtreat", "RTtreat", "destinationPlotID", "turfID", "subPlot", "year", "date", "Measure", "recorder")
+
+envNames <- c("moss", "lichen", "litter", "soil", "rock", "totalVascular", "totalBryophytes", "totalLichen", "vegetationHeight", "mossHeight", "litterThickness", "comment")
+
+sppNames <- names(allsites)[!names(allsites) %in% c(metaNames, envNames)]
+sppNames <- sort(sppNames)
+sppNames
+
+allsites <- allsites[,  c(metaNames, sppNames, envNames)]
+
+names(allsites)
+
+#check for odditities
+allsites[, sppNames] %>% 
+  tidyr::gather(key = spp, value = cover) %>%
+  mutate(cover = as.character(cover)) %>% 
+  filter(!is.na(cover)) %>% 
+  mutate(cover = trimws(cover)) %>% 
+  filter(!grepl("^\\d+(?:\\.\\d+)?$", cover))
+
+#trim anywhite space
+allsites <- plyr::colwise(trimws)(allsites)
+#ensure numeric
+allsites[, sppNames] <- plyr::colwise(as.numeric)(allsites[, sppNames])
+
+##write data to csv file
+write.csv(allsites, file = "community/databaseSetup/data/allsites.csv", row.names = FALSE)
