382a887b75759c351eba82297dd2110895aa9ab4
richardjtelford <richard.telford@bio.uib.no>
2020-04-29 17:50:54 +0200

    fix typo

--- R/bootstrap_moment_plan.R
+++ R/bootstrap_moment_plan.R
@@ -7,7 +7,7 @@ bootstrap_moment_plan <- drake_plan(
     imputed_traits_home <- community %>%
       filter(year == min(year) | TTtreat %in% c("control", "local", "OTC")) %>% 
       select(Site = originSiteID, Location = originBlockID, turfID, year, TTtreat, Taxon = speciesName, cover) %>% 
-      trait_impute(traits = trait_trans, 
+      trait_impute(traits = traits, 
                    scale_hierarchy = c("Site", "Location"),
                    taxon_col = "Taxon", 
                    value_col = "value_trans", 
@@ -17,7 +17,7 @@ bootstrap_moment_plan <- drake_plan(
     imputed_traits_transplant <- community %>%
       filter(year > min(year), !TTtreat %in% c("control", "local", "OTC")) %>%
       select(Site = destSiteID, Location = destBlockID, turfID, year, TTtreat, Taxon = speciesName, cover) %>% 
-      trait_impute(traits = trait_trans, 
+      trait_impute(traits = traits, 
                    scale_hierarchy = c("Site", "Location"),
                    taxon_col = "Taxon",
                    value_col = "value_trans",
