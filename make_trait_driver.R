4fbe64659ef8ce8151a350c0afbe39d95a4b5cdc
richardjtelford <richard.telford@bio.uib.no>
2020-07-13 22:52:58 +0200

    move main plan to R directory

--- make_trait_driver.R
+++ make_trait_driver.R
@@ -1,10 +1,12 @@
 #load packages
 library("drake")
 
+#plan
+trait_driver_plan <- "R/1_trait_driver_drake.R"
 
 #Build the right things
-r_make(source = "trait_driver_drake.R")
+r_make(source = trait_driver_plan)
 drake_failed()
 
 #view dependency graph
-r_vis_drake_graph(source = "trait_driver_drake.R", targets_only = TRUE)
+r_vis_drake_graph(source = trait_driver_plan, targets_only = TRUE)
