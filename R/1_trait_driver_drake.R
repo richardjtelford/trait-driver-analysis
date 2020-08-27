6eadd3d49b88878c1b7e1b2c600b95936490041e
richardjtelford <richard.telford@bio.uib.no>
2020-08-27 12:22:05 +0200

    methods and results ms

--- R/1_trait_driver_drake.R
+++ R/1_trait_driver_drake.R
@@ -36,13 +36,32 @@ analysis_plan <- drake_plan(
   
 )
 
+# manuscript plan
+manuscript_plan <- drake_plan(
+  #add extra packages to bibliography
+  biblio2 = package_citations(
+    packages = c("e1071", "traitstrap", "drake", "tidyverse", "rmarkdown", "renv"), 
+    old_bib = file_in("Rmd/TDT.bib"), 
+    new_bib = file_out("Rmd/TDT2.bib")),
+  
+  #knit manuscript
+  manuscript = {
+    file_in("Rmd/elsevier-harvard_rjt.csl")
+    file_in("Rmd/TDT2.bib")
+    rmarkdown::render(
+      input = knitr_in("methods_results.Rmd"), 
+      clean = FALSE)
+  }
+)
+
 #### combine plans ####
 trait_plan <- bind_plans(download_plan, 
                         import_plan, 
                         bootstrap_moment_plan,
                         euclidean_distance_plan,
                         analysis_plan, 
-                        plot_plan)
+                        plot_plan, 
+                        manuscript_plan)
 #quick plot
 plot(trait_plan)
 
--- make_trait_driver.R
+++ make_trait_driver.R
@@ -8,5 +8,11 @@ trait_driver_plan <- "R/1_trait_driver_drake.R"
 r_make(source = trait_driver_plan)
 drake_failed()
 
+# show methods results
+if(length(drake_failed()) == 0){
+  fs::file_show("methods_results.pdf")#display pdf
+}
+
+
 #view dependency graph
 r_vis_drake_graph(source = trait_driver_plan, targets_only = TRUE)
