6ad83ee7c816900bfa8c3e2c161a8d9767de0ad8
audhalbritter <me@audhalbritter.com>
2020-10-26 16:19:05 +0100

    streamline rda analysis, removing nr of onjects

--- R/rda_plan.R
+++ R/rda_plan.R
@@ -14,12 +14,11 @@ rda_plan <- drake_plan(
            year = factor(year),
            TTtreat = factor(TTtreat, levels = c("control", "OTC", "warm1", "warm3", "control_L"))),
   
-  #only traits
-  trait_warm_data_fixed = trait_warm_fat_fixed %>% 
-    select(C_percent:Wet_Mass_g_log),
-  
   #prc
-  fit_Warming_fixed = prc(response = trait_warm_data_fixed, treatment = trait_warm_fat_fixed$TTtreat, time = trait_warm_fat_fixed$year, scale = TRUE),
+  fit_Warming_fixed = prc(response = trait_warm_fat_fixed %>% select(C_percent:Wet_Mass_g_log), 
+                          treatment = trait_warm_fat_fixed$TTtreat, 
+                          time = trait_warm_fat_fixed$year, 
+                          scale = TRUE),
   
   #make plots
   wf2 = autoplot.prcWithoutSP(fit_Warming_fixed, xlab = "", ylab = "Treatment effect on \n  trait composition") +
@@ -36,7 +35,7 @@ rda_plan <- drake_plan(
     filter(Score == "Species") %>% 
     mutate(X = 1) %>% 
     ggplot(aes(x = X, y = (Response), label = Label)) +
-    geom_text(aes(x = X), size = 2.5) +
+    geom_text(aes(x = X), size = 1.5) +
     geom_hline(yintercept = 0) +
     scale_y_continuous(breaks = pretty(fortify(fit_Warming_fixed)$Response, n = 5)) +
     labs(x = "", y = "") +
@@ -58,12 +57,11 @@ rda_plan <- drake_plan(
            year = factor(year),
            TTtreat = factor(TTtreat, levels = c("control", "OTC", "warm1", "warm3", "control_L"))),
   
-  #only traits
-  trait_warm_data_plastic = trait_warm_fat_plastic %>% 
-    select(C_percent:Wet_Mass_g_log),
-  
   #prc
-  fit_Warming_plastic = prc(response = trait_warm_data_plastic, treatment = trait_warm_fat_plastic$TTtreat, time = trait_warm_fat_plastic$year, scale = TRUE),
+  fit_Warming_plastic = prc(response = trait_warm_fat_plastic %>% select(C_percent:Wet_Mass_g_log),
+                            treatment = trait_warm_fat_plastic$TTtreat, 
+                            time = trait_warm_fat_plastic$year, 
+                            scale = TRUE),
   
   #make plots
   wp2 = autoplot.prcWithoutSP(fit_Warming_plastic, xlab = "", ylab = "Treatment effect on \n  trait composition") +
@@ -79,7 +77,7 @@ rda_plan <- drake_plan(
     filter(Score == "Species") %>% 
     mutate(X = 1) %>% 
     ggplot(aes(x = X, y = (Response), label = Label)) +
-    geom_text(aes(x = X), size = 2.5) +
+    geom_text(aes(x = X), size = 1.5) +
     geom_hline(yintercept = 0) +
     scale_y_continuous(breaks = pretty(fortify(fit_Warming_plastic)$Response, n = 5)) +
     labs(x = "", y = "") +
@@ -102,11 +100,11 @@ rda_plan <- drake_plan(
            year = factor(year),
            TTtreat = factor(TTtreat, levels = c("control", "cool1", "cool3", "control_H"))),
   
-  #only traits
-  trait_cool_data_fixed = trait_cool_fat_fixed %>% select(C_percent:Wet_Mass_g_log),
-  
   #prc
-  fit_Cool_fixed = prc(response = trait_cool_data_fixed, treatment = trait_cool_fat_fixed$TTtreat, time = trait_cool_fat_fixed$year, scale = TRUE),
+  fit_Cool_fixed = prc(response = trait_cool_fat_fixed %>% select(C_percent:Wet_Mass_g_log),
+                       treatment = trait_cool_fat_fixed$TTtreat,
+                       time = trait_cool_fat_fixed$year,
+                       scale = TRUE),
   
   #make plots
   cf2 = autoplot.prcWithoutSP(fit_Cool_fixed, xlab = "", ylab = "") +
@@ -123,7 +121,7 @@ rda_plan <- drake_plan(
     filter(Score == "Species") %>% 
     mutate(X = 1) %>% 
     ggplot(aes(x = X, y = Response, label = Label)) +
-    geom_text(aes(x = X), size = 2.5) +
+    geom_text(aes(x = X), size = 1.5) +
     geom_hline(yintercept = 0) +
     scale_y_continuous(breaks = pretty(fortify(fit_Cool_fixed)$Response, n = 5)) +#, trans = "reverse") +
     labs(x = "", y = "") +
@@ -145,11 +143,11 @@ rda_plan <- drake_plan(
            year = factor(year),
            TTtreat = factor(TTtreat, levels = c("control", "cool1", "cool3", "control_H"))),
   
-  #only traits
-  trait_cool_data_plastic = trait_cool_fat_plastic %>% select(C_percent:Wet_Mass_g_log),
-  
   #prc
-  fit_Cool_plastic = prc(response = trait_cool_data_plastic, treatment = trait_cool_fat_plastic$TTtreat, time = trait_cool_fat_plastic$year, scale = TRUE),
+  fit_Cool_plastic = prc(response = trait_cool_fat_plastic %>% select(C_percent:Wet_Mass_g_log),
+                         treatment = trait_cool_fat_plastic$TTtreat,
+                         time = trait_cool_fat_plastic$year,
+                         scale = TRUE),
   
   #make plots
   cp2 = autoplot.prcWithoutSP(fit_Cool_plastic, xlab = "", ylab = "") +
@@ -165,52 +163,94 @@ rda_plan <- drake_plan(
     filter(Score == "Species") %>% 
     mutate(X = 1) %>% 
     ggplot(aes(x = X, y = Response, label = Label)) +
-    geom_text(aes(x = X), size = 2.5) +
+    geom_text(aes(x = X), size = 1.5) +
     geom_hline(yintercept = 0) +
     scale_y_continuous(breaks = pretty(fortify(fit_Cool_plastic)$Response, n = 5)) +#, trans = "reverse") +
     labs(x = "", y = "") +
     theme(panel.background = element_blank(),
           axis.ticks.x = element_blank(),
           axis.text.x = element_blank()),
-  
-  TraitRDA_Cooling_plastic = gridExtra::grid.arrange(cp2, cp3,
-                                                   layout_matrix = rbind(c(1,1,1,1,1,2,2))),
-  
+
   #patchwork together
   TraitRDA = ((wf2 + wf3 + wp2 + wp3) + plot_layout(widths = c(4, 1, 4, 1))) / ((cf2 + cf3 + cp2 + cp3) + plot_layout(widths = c(4, 1, 4, 1))),
 
   
   #permutation test
-  #warm - fixed
-  trait_warm_fat_fixed2 = trait_warm_fat_fixed %>% 
-    filter(TTtreat != "control_L"),
-  trait_warm_data_fixed2 = trait_warm_fat_fixed2 %>% 
-    select(-(originSiteID:destSiteID)),
+  #warm
+  rda_warm = bind_rows(
+    fixed = trait_warm_fat_fixed,
+    plastic = trait_warm_fat_plastic,
+    .id = "Plasticity"
+  ) %>% 
+    filter(TTtreat != "control_L") %>% 
+    nest(data = -Plasticity) %>% 
+    mutate(mod = map(data, ~ prc(response = .x %>% select(C_percent:Wet_Mass_g_log),
+                                       treatment = .x$TTtreat,
+                                       time = .x$year,
+                                       scale = TRUE))) %>% 
+    mutate(res = map(mod, anova),
+           Variance = map(res, "Variance") %>% map_dbl(1),
+           Fvalue = map(res, "F") %>% map_dbl(1),
+           Pvalue = map(res, "Pr(>F)") %>% map_dbl(1)) %>% 
+    select(-data, -mod, -res),
   
-  perm_warm_fixed = anova(prc(response = trait_warm_data_fixed2, treatment = trait_warm_fat_fixed2$TTtreat, time = trait_warm_fat_fixed2$year, scale = TRUE)),
+  #cool
+  rda_cool = bind_rows(
+    fixed = trait_warm_fat_fixed,
+    plastic = trait_warm_fat_plastic,
+    .id = "Plasticity"
+  ) %>% 
+    filter(TTtreat != "control_H") %>% 
+    nest(data = -Plasticity) %>% 
+    mutate(mod = map(data, ~ prc(response = .x %>% select(C_percent:Wet_Mass_g_log),
+                                 treatment = .x$TTtreat,
+                                 time = .x$year,
+                                 scale = TRUE))) %>% 
+    mutate(res = map(mod, anova),
+           Variance = map(res, "Variance") %>% map_dbl(1),
+           Fvalue = map(res, "F") %>% map_dbl(1),
+           Pvalue = map(res, "Pr(>F)") %>% map_dbl(1)) %>% 
+    select(-data, -mod, -res)
   
-  #warm - plastic
-  trait_warm_fat_plastic2 = trait_warm_fat_plastic %>% 
-    filter(TTtreat != "control_L"),
-  trait_warm_data_plastic2 = trait_warm_fat_plastic2 %>% 
-    select(-(originSiteID:destSiteID)),
   
-  perm_warm_plastic = anova(prc(response = trait_warm_data_plastic2, treatment = trait_warm_fat_plastic2$TTtreat, time = trait_warm_fat_plastic2$year, scale = TRUE)),
   
-  #cool - fixed
-  trait_cool_fat_fixed2 = trait_cool_fat_fixed %>% 
-    filter(TTtreat != "control_H"),
-  trait_cool_data_fixed2 = trait_cool_fat_fixed2 %>% 
-    select(-(originSiteID:destSiteID)),
   
-  perm_cool_fixed = anova(prc(response = trait_cool_data_fixed2, treatment = trait_cool_fat_fixed2$TTtreat, time = trait_cool_fat_fixed2$year, scale = TRUE)),
   
-  #cool - plastic
-  trait_cool_fat_plastic2 = trait_cool_fat_plastic %>% 
-    filter(TTtreat != "control_H"),
-  trait_cool_data_plastic2 = trait_cool_fat_plastic2 %>% 
-    select(-(originSiteID:destSiteID)),
   
-  perm_cool_plastic = anova(prc(response = trait_cool_data_plastic2, treatment = trait_cool_fat_plastic2$TTtreat, time = trait_cool_fat_plastic2$year, scale = TRUE))
+  # #warm - fixed
+  # trait_warm_fat_fixed2 = trait_warm_fat_fixed %>% 
+  #   filter(TTtreat != "control_L"),
+  # 
+  # perm_warm_fixed = anova(prc(response = trait_warm_fat_fixed2 %>% select(-(originSiteID:destSiteID)),
+  #                             treatment = trait_warm_fat_fixed2$TTtreat,
+  #                             time = trait_warm_fat_fixed2$year,
+  #                             scale = TRUE)),
+  # 
+  # #warm - plastic
+  # trait_warm_fat_plastic2 = trait_warm_fat_plastic %>% 
+  #   filter(TTtreat != "control_L"),
+  # 
+  # perm_warm_plastic = anova(prc(response = trait_warm_fat_plastic2 %>% select(-(originSiteID:destSiteID)),
+  #                               treatment = trait_warm_fat_plastic2$TTtreat,
+  #                               time = trait_warm_fat_plastic2$year,
+  #                               scale = TRUE)),
+  # 
+  # #cool - fixed
+  # trait_cool_fat_fixed2 = trait_cool_fat_fixed %>% 
+  #   filter(TTtreat != "control_H"),
+  # 
+  # perm_cool_fixed = anova(prc(response = trait_cool_fat_fixed2 %>% select(-(originSiteID:destSiteID)),
+  #                             treatment = trait_cool_fat_fixed2$TTtreat,
+  #                             time = trait_cool_fat_fixed2$year,
+  #                             scale = TRUE)),
+  # 
+  # #cool - plastic
+  # trait_cool_fat_plastic2 = trait_cool_fat_plastic %>% 
+  #   filter(TTtreat != "control_H"),
+  # 
+  # perm_cool_plastic = anova(prc(response = trait_cool_fat_plastic2 %>% select(-(originSiteID:destSiteID)),
+  #                               treatment = trait_cool_fat_plastic2$TTtreat,
+  #                               time = trait_cool_fat_plastic2$year,
+  #                               scale = TRUE))
   
 )
