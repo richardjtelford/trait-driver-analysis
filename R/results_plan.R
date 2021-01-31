8b088d4a14d791fb862df7113e44cfcaf832a2ea
audhalbritter <me@audhalbritter.com>
2021-01-31 16:29:05 +0100

    remove happy moment by year stats

--- R/results_plan.R
+++ R/results_plan.R
@@ -1,7 +1,7 @@
 results_plan <- drake_plan(
   
   #trait climate regressions table
-  moments_by_climate_table = trait_climate_regression %>% 
+  moments_by_climate_table = trait_climate_regression %>% ungroup() %>% distinct(trait_trans)
     mutate(trait_trans = factor(trait_trans, levels = c("dN15_permil", "Leaf_Area_cm2_log", "Dry_Mass_g_log", "C_percent", "SLA_cm2_g", "NP_ratio", "LDMC", "P_percent", "N_percent", "dC13_permil", "Thickness_mm_log", "CN_ratio"))) %>% 
     select(trait_trans, term:p.value) %>% 
     mutate(estimate = round(estimate, 2),
@@ -48,18 +48,18 @@ results_plan <- drake_plan(
     knitr::kable(),
   
   #happymoments results table
-  happymoment_effect_table = happymoment_effect %>% 
-    select(plasticity:happymoment, term:p.value) %>% 
-    mutate(term = plyr::mapvalues(term, from = c("Tcontrol", "Tcool1", "Tcool3", "TOTC", "Twarm1", "Twarm3", "control", "cool1", "cool3", "OTC", "warm1", "warm3"),
-                                  to = c("control", "cool1", "cool3", "OTC", "warm1", "warm3", "control*year", "cool1*year", "cool3*year", "OTC*year", "warm1*year", "warm3*year"))) %>%
-    mutate(estimate = round(estimate, 2),
-           std.error = round(std.error, 2),
-           statistic = round(statistic, 2),
-           p.value = round(p.value, 3),
-           p.value = case_when(p.value < 0.001 ~ paste(p.value, "***"),
-                               p.value < 0.01 ~ paste(p.value, "**"),
-                               p.value < 0.05 ~ paste(p.value, "*"),
-                               p.value >= 0.05 ~ paste(p.value, ""))) %>% 
-    knitr::kable()
+  # happymoment_effect_table = happymoment_effect %>% 
+  #   select(plasticity:happymoment, term:p.value) %>% 
+  #   mutate(term = plyr::mapvalues(term, from = c("Tcontrol", "Tcool1", "Tcool3", "TOTC", "Twarm1", "Twarm3", "control", "cool1", "cool3", "OTC", "warm1", "warm3"),
+  #                                 to = c("control", "cool1", "cool3", "OTC", "warm1", "warm3", "control*year", "cool1*year", "cool3*year", "OTC*year", "warm1*year", "warm3*year"))) %>%
+  #   mutate(estimate = round(estimate, 2),
+  #          std.error = round(std.error, 2),
+  #          statistic = round(statistic, 2),
+  #          p.value = round(p.value, 3),
+  #          p.value = case_when(p.value < 0.001 ~ paste(p.value, "***"),
+  #                              p.value < 0.01 ~ paste(p.value, "**"),
+  #                              p.value < 0.05 ~ paste(p.value, "*"),
+  #                              p.value >= 0.05 ~ paste(p.value, ""))) %>% 
+  #   knitr::kable()
   
 )
